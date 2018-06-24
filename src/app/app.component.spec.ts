9811e0fa8f6a8562851a5d4ecfed44aed83d319d
Jordan Bonser <jordan_bonser@live.co.uk>
2018-06-24 12:41:13 +0100

    Added a Message Service
    The Message Service is used by the Hero service
    and can be used by other services/components to
    output their own messages.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -12,7 +13,8 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
         HeroesComponent,
-        HeroDetailComponent
+        HeroDetailComponent,
+        MessagesComponent
       ],
     }).compileComponents();
   }));
