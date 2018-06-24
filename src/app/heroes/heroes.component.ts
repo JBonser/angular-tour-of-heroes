d3dcfe95f5850cfee2d377dd4cc6d600b7980845
Jordan Bonser <jordan_bonser@live.co.uk>
2018-06-24 11:46:45 +0100

    Added Hero List and selector
    Added a list of mock heroes that I used to generate a List
    of heroes on the page. Once a hero is selected then the
    details of that hero appear and the hero is highlighted
    in the list below.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +10,14 @@ import { Hero } from '../Hero';
 
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
+  selectedHero: Hero;
+  heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
